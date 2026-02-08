d238d31261008f7bd55173546b8ed8e521066ac3
Hossain Khan <hello@hossain.dev>
2026-02-07 20:54:04 -0500

    [MINOR] Formatting

--- app/src/main/java/ink/trmnl/android/ui/aboutapp/AppInfoScreen.kt
+++ app/src/main/java/ink/trmnl/android/ui/aboutapp/AppInfoScreen.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
